be909da1fb58a95985ba2bb0ab0820bac1c35f73
Mehdi <moosorkh@gmail.com>
2022-07-04 18:27:13 -0700

    findLongestSubstring

---
+++ findLongestSubString.js
@@ -0,0 +1,17 @@
+function findLongestSubstring(str){
+    let start = 0;
+    let map = {};
+    let longest = 0;
+    for(let i  = 0; i < str.length; i++){
+        let char = str[i];
+        if(map[char]){
+            start = Math.max(start, map[char]);
+        }
+        longest = Math.max(longest, i - start + 1);
+        map[char] = i + 1;
+    }
+    return longest;
+}
+
+console.log(findLongestSubstring("string")); // returns 6 since none of the contigous characters are repeated.
+console.log(findLongestSubstring("text")); // returns 3 since there are only 3 non-repeated contigous characters
