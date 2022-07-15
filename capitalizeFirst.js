880ab7e9def5ea3f2b8c26db523956bdb925f994
Moosorkh <moosorkh@gmail.com>
2022-07-15 15:23:09 -0700

    capitalizeFirst Completed

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,6 +1,11 @@
 function capitalizeFirst(array){
     if(array.length === 1){
-        return [array[0][0].toUpperCase()+ array[0].substr(1)];
+        return [array[0][0].toUpperCase()+ array[0].substring(1)];
     }
-    
+    const res = capitalizeFirst(array.slice(0, -1));
+    const string = array.slice(array.length -1)[0][0].toUpperCase()+ array.slice(array.length-1)[0].substring(1);
+    res.push(string);
+    return res;
 }
+
+console.log(capitalizeFirst(['cat', 'dog']));
