fdadcdbe142fe2f7c5d53410a0fb75ce85bff8eb
Mehdi <moosorkh@gmail.com>
2022-07-17 01:11:42 -0700

    capitalizeFirst done

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,11 +1,17 @@
+//Given an array of strings, capitalize the first letter of each string in the array
+
 function capitalizeFirst(array){
     if(array.length === 1){
-        return [array[0][0].toUpperCase()+ array[0].substring(1)];
+        return [array.slice(array.length -1)[0][0].toUpperCase() + array[0].substring(1)];
     }
     const res = capitalizeFirst(array.slice(0, -1));
-    const string = array.slice(array.length -1)[0][0].toUpperCase()+ array.slice(array.length-1)[0].substring(1);
+    const string = array.slice(array.length -1)[0][0].toUpperCase() + array.slice(array.length -1)[0].substring(1);
     res.push(string);
     return res;
 }
 
-console.log(capitalizeFirst(['cat', 'dog', 'human', 'tiger']));
+
+
+let arr = ['cat', 'dog', 'human', 'tiger'];
+
+console.log(capitalizeFirst(arr)); // will return [ 'Cat', 'Dog', 'Human', 'Tiger' ]
