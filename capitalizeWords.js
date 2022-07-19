823d2ae0555c40414a40d768f1b69ef9b042b519
Mehdi <moosorkh@gmail.com>
2022-07-18 22:45:40 -0700

    capitalizeWords

---
+++ capitalizeWords.js
@@ -0,0 +1,11 @@
+//function to capitalize all the word elements of an array
+function capitalizeWords(array){
+    if(array.length === 1){
+        return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length -1)[0].toUpperCase());
+    return res;
+}
+
+console.log(capitalizeWords(['cat', 'dog', 'fish'])); // returns [ 'CAT', 'DOG', 'FISH' ]
