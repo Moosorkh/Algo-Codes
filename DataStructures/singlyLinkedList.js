46805a06d43012cc4cb8f203c536aefef2af2b8c
Fortis Irdmousa <moosorkh@gmail.com>
2022-10-13 07:19:39 -0700

    Unshift added

--- DataStructures/singlyLinkedList.js
+++ DataStructures/singlyLinkedList.js
@@ -54,4 +54,18 @@ class sinlgyLinkedList{
         }
         return currentHead;
     }
+
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
